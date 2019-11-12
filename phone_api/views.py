70eaf600f5beda749e7cbae83bccd0c493b46fea
X-X-Xavito <kaiquexatalla@gmail.com>
2019-11-11 22:48:52 -0200

    Create company list and detail views
    The product views will come later

--- phone_api/views.py
+++ phone_api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from phone_api.models import Company, Product
+from phone_api.serializers import CompanySerializer, ProductSerializer
+
+@csrf_exempt
+def company_list(request):
+    """
+    List all companies, or create a new one.
+    """
+    if request.method == 'GET':
+        companies = Company.objects.all()
+        company_serializer = CompanySerializer(companies, many=True)
+        return JsonResponse(company_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        company_serializer = CompanySerializer(data=data)
+        if company_serializer.is_valid():
+            company_serializer.save()
+            return JsonResponse(company_serializer.data, status=201)
+        return JsonResponse(company_serializer.errors, status=400)
+
+
+@csrf_exempt
+def company_detail(request, company_id):
+    """
+    Retrieve, update or delete a company.
+    """
+    try:
+        company = Company.objects.get(company_id=company_id)
+    except Company.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        company_serializer = CompanySerializer(company)
+        return JsonResponse(company_serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        company_serializer = CompanySerializer(company, data=data)
+        if company_serializer.is_valid():
+            company_serializer.save()
+            return JsonResponse(company_serializer.data)
+        return JsonResponse(company_serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        company.delete()
+        return HttpResponse(status=204)
