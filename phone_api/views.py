661436bc3961c7d348415f5f4af124bec1e7fd88
X-X-Xavito <kaiquexatalla@gmail.com>
2019-11-13 21:59:31 -0200

    Add permission and auth handlers

--- phone_api/views.py
+++ phone_api/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 
-from rest_framework import generics, mixins, status
+from rest_framework import generics, mixins, status, permissions
 from rest_framework.response import Response
 
 from phone_api.models import Company, Product, Recharge
@@ -9,10 +9,12 @@ from phone_api.serializers import CompanySerializer, ProductSerializer, Recharge
 class CompanyListAPIView(generics.ListAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class CompanyRetDesAPIView(generics.RetrieveDestroyAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         company_id = self.kwargs.get('company_id')
@@ -24,10 +26,12 @@ class CompanyRetDesAPIView(generics.RetrieveDestroyAPIView):
 class CompanyCreateAPIView(generics.CreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated]
     
 class ProductCreateApiView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(company_id=self.kwargs.get('company_id'))
@@ -37,6 +41,7 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
     lookup_url_kwarg = 'product_id'
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_update(self, serializer):
         serializer.save(product_id=self.kwargs.get('product_id'))
@@ -45,14 +50,17 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
     lookup_field = 'product_id'
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class RechargeCreateAPIView(generics.CreateAPIView):
     queryset = Recharge.objects.all()
     serializer_class = RechargeSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class RechargeListAPIView(generics.ListAPIView):
     lookup_field='phone_number'
     serializer_class = RechargeSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         phone_number = self.kwargs.get('phone_number')
@@ -61,6 +69,7 @@ class RechargeListAPIView(generics.ListAPIView):
 class RechargeRetriveAPIView(generics.RetrieveAPIView):
     lookup_field='id'
     serializer_class = RechargeSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         recharge_id = self.kwargs.get('pk')
