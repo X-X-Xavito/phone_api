e8ffbadd30b3519c0988b004b5145334e8d49ada
X-X-Xavito <kaiquexatalla@gmail.com>
2019-11-12 23:56:25 -0200

    Create new Urls paths
    This paths will handle the CRUD methods

--- phone_api/urls.py
+++ phone_api/urls.py
@@ -3,7 +3,15 @@ from phone_api import views
 
 
 urlpatterns=[
-    path('CompanyProducts/', views.CompanyList.as_view()),
-    path('CompanyProducts/<str:company_id>/', views.CompanyDetail.as_view()),
-    path('CreateProduct/<str:company_id>/', views.CreateProduct.as_view()), 
+    path('CompanyProducts/', views.CompanyListAPIView.as_view()), #List
+
+    path('CompanyProducts/<str:company_id>/', views.CompanyRetDesAPIView.as_view()), #Retrive and Destroy
+
+    path('CreateCompany/', views.CompanyCreateAPIView.as_view()), #Create Company
+
+    path('<str:company_id>/CreateProduct/', views.ProductCreateApiView.as_view()),
+
+    path('UpdateProduct/<str:product_id>/', views.ProductUpdateAPIView.as_view()), #Update Product
+    
+    path('DeleteProduct/<str:product_id>/', views.ProductDeleteAPIView.as_view()), #Delete Product
 ]
