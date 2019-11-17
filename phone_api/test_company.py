ebb6eacefcbd58f6f9e7096aae10a0bf14affabd
X-X-Xavito <kaiquexatalla@gmail.com>
2019-11-17 18:10:41 -0200

    Add new tests cases
    Also added a new pytest fixture for recharge

--- phone_api/test_company.py
+++ phone_api/test_company.py
@@ -2,6 +2,7 @@ import pytest
 
 from .models import Company, Product, Recharge
 
+pytestmark = pytest.mark.django_db
 
 @pytest.fixture
 def test_company():
@@ -16,9 +17,27 @@ def test_product():
     """
         Return a product instance
     """
-    company = test_company()
+    company = Company(company_id="test_company")
     product = Product(product_id="test_product", company_id=company, value=10.0)
     return product
 
+
+@pytest.fixture
+def test_recharge():
+    """
+        Return a recharge instance
+    """
+    company = Company(company_id="test_company")
+    product = Product(product_id="test_product", company_id=company, value=10.0)
+    recharge = Recharge(company_id=company, product_id=product, phone_number="5511999999999", value=product.value)
+    return recharge
+
 def test_company_company_id(test_company):
-    assert test_company.company_id is str
+    assert type(test_company.company_id) is str
+
+def test_product_product_id(test_product):
+    assert type(test_product.product_id) is str
+
+def test_recharge_attr(test_recharge):
+    assert type(test_recharge.phone_number) is str
+    assert type(test_recharge.value) is float
