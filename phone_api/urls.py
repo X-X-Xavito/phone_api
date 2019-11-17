65fe1cc929c90b0b0e0a7b61d018ba211b7d2a44
X-X-Xavito <kaiquexatalla@gmail.com>
2019-11-17 18:45:26 -0200

    Add name to the urls

--- phone_api/urls.py
+++ phone_api/urls.py
@@ -3,22 +3,22 @@ from phone_api import views
 
 
 urlpatterns=[
-    path('CompanyProducts/', views.CompanyListAPIView.as_view()), #List
+    path('CompanyProducts/', views.CompanyListAPIView.as_view(), name='company-products'), #List
 
-    path('CompanyProducts/<str:company_id>/', views.CompanyRetDesAPIView.as_view()), #Retrive and Destroy
+    path('CompanyProducts/<str:company_id>/', views.CompanyRetDesAPIView.as_view(), name='company-products-id'), #Retrive and Destroy
 
-    path('CreateCompany/', views.CompanyCreateAPIView.as_view()), #Create Company
+    path('CreateCompany/', views.CompanyCreateAPIView.as_view(), name='create-company'), #Create Company
 
-    path('<str:company_id>/CreateProduct/', views.ProductCreateApiView.as_view()),
+    path('<str:company_id>/CreateProduct/', views.ProductCreateApiView.as_view(), name='str-create-product'),
 
-    path('UpdateProduct/<str:product_id>/', views.ProductUpdateAPIView.as_view()), #Update Product
+    path('UpdateProduct/<str:product_id>/', views.ProductUpdateAPIView.as_view(), name='update-product'), #Update Product
     
-    path('DeleteProduct/<str:product_id>/', views.ProductDeleteAPIView.as_view()), #Delete Product
+    path('DeleteProduct/<str:product_id>/', views.ProductDeleteAPIView.as_view(), name='delete-product'), #Delete Product
 
-    path('PhoneRecharges/', views.RechargeCreateAPIView.as_view()), #Create Recharge
+    path('PhoneRecharges/', views.RechargeCreateAPIView.as_view(), name='phone-recharges'), #Create Recharge
     
-    path('PhoneRecharges/id/<int:pk>/', views.RechargeRetriveAPIView.as_view()), #Retrieve single Recharge
+    path('PhoneRecharges/id/<int:pk>/', views.RechargeRetriveAPIView.as_view(), name='phone-recharges-id'), #Retrieve single Recharge
 
-    path('PhoneRecharges/<str:phone_number>/', views.RechargeListAPIView.as_view()), #List Recharges
+    path('PhoneRecharges/<str:phone_number>/', views.RechargeListAPIView.as_view(), name='phone-recharges-str'), #List Recharges
 
 ]
